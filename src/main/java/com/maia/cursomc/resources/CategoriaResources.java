81f62e9d12c4132b084a178022a0457a400b4166
DowglasMaia <luciodowglas@live.com>
2018-08-15 09:59:33 -0300

    Testatndo a 01 Class do domian

--- src/main/java/com/maia/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/maia/cursomc/resources/CategoriaResources.java
@@ -1,15 +1,27 @@
 package com.maia.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.maia.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaResources {
 
 	@GetMapping
-	public String listar() {
-		return "Rest estar Funcionado";
+	public List<Categoria> listar() {
+		Categoria cat1 = new Categoria(1, "Cursos");
+		Categoria cat2 = new Categoria(2, "Escritório");
+
+		List<Categoria> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+
+		return list;
 	}
 }
