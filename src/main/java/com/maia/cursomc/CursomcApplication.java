b671bddc2aac15aeb9deff7855ef9f5585a7fae0
DowglasMaia <luciodowglas@live.com>
2018-08-16 09:41:13 -0300

    Asociação Muitos para Muitos - Produtos e Categorias

--- src/main/java/com/maia/cursomc/CursomcApplication.java
+++ src/main/java/com/maia/cursomc/CursomcApplication.java
@@ -8,13 +8,19 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.maia.cursomc.domain.Categoria;
+import com.maia.cursomc.domain.Produto;
 import com.maia.cursomc.repositores.CategoriaRepository;
+import com.maia.cursomc.repositores.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
 
+	// Dependencia de Categoria
 	@Autowired
 	CategoriaRepository repository;
+	// Dependencia de Produto
+	@Autowired
+	ProdutoRepository pdtRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -22,10 +28,26 @@ public class CursomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
+		// Categorias
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Telefonia");
 
+		// Produtos
+		Produto p1 = new Produto(null, "Notbook S50", 6000.00);
+		Produto p2 = new Produto(null, "Placa Grafica GTX1080TI", 5090.00);
+		Produto p3 = new Produto(null, "Samsung S8 Plus", 2999.00);
+
+		// Assoçiando a Listas
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
+		cat2.getProdutos().addAll(Arrays.asList(p3));
+
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1));
+		p3.getCategorias().addAll(Arrays.asList(cat2, cat1));
+
+		// **Salvando as Listas de Produto e Categorias no BD
 		repository.saveAll(Arrays.asList(cat1, cat2));
+		pdtRepository.saveAll(Arrays.asList(p1, p2, p3));
 	}
 
 }
---
+++ src/main/java/com/maia/cursomc/repositores/ProdutoRepository.java
@@ -0,0 +1,11 @@
+package com.maia.cursomc.repositores;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.maia.cursomc.domain.Produto;
+
+@Repository
+public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+}
