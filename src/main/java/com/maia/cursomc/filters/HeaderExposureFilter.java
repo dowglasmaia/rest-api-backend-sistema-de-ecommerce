33f7f5806e8c75189e3a237b22b8dac02a572404
Dowglas Maia <dowglasmaia@gmail.com>
2018-12-30 10:08:23 -0200

    Expondo Header location, nas resposta

---
+++ src/main/java/com/maia/cursomc/filters/HeaderExposureFilter.java
@@ -0,0 +1,41 @@
+package com.maia.cursomc.filters;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Component;
+
+/*Tornando o Header location Visivel - para que minha aplicação Angular tenha acesso a este cabeçalho*/
+
+@Component
+public class HeaderExposureFilter implements Filter {
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletResponse res = (HttpServletResponse) response;
+		res.addHeader("access-control-expose-headers", "location"); //libera a Leitura Cabeçalho do Location
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+		
+
+	}
+
+}
