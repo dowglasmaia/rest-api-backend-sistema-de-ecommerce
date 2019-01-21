e0d56cdc454986cace2ce0eaf711dc02a84767b9
Dowglas Maia <dowglasmaia@gmail.com>
2019-01-20 22:38:25 -0200

    Atualizando JWTAuthentication, Excption de Responsta 403 para 401

--- src/main/java/com/maia/cursomc/domain/security/JWTAuthenticationFilter.java
+++ src/main/java/com/maia/cursomc/domain/security/JWTAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.maia.cursomc.domain.security;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Date;
 
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
@@ -12,6 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -24,6 +26,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 	private JWTUtil jwtUtil;
 
 	public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+		setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
 		this.authenticationManager = authenticationManager;
 		this.jwtUtil = jwtUtil;
 	}
@@ -52,7 +55,29 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSS) auth.getPrincipal()).getUsername();
 		String token = jwtUtil.generateToken(username);
 		res.addHeader("Authorization", "Bearer " + token);
-		res.addHeader("access-control-expose-headers", "Authorization"); //libera a Leitura do Authorization por CORS
+		res.addHeader("access-control-expose-headers", "Authorization"); // libera a Leitura do Authorization por CORS
+	}
+
+	/* == Correção de Erro de Resposta de Error 403 para 401 == */
+	private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+		@Override
+		public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+				AuthenticationException exception) throws IOException, ServletException {
+			response.setStatus(401);
+			response.setContentType("application/json");
+			response.getWriter().append(json());
+		}
+
+		private String json() {
+			long date = new Date().getTime();
+			return "{\"timestamp\": " 
+					+ date + ", " 
+					+ "\"status\": 401, " 
+					+ "\"error\": \"Não autorizado\", "
+					+ "\"message\": \"Email ou senha inválidos\", " 
+					+ "\"path\": \"/login\"}";
+		}
 	}
 
 }
